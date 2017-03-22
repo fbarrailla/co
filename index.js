ca31d4b5c6b5b9635695fc2de45d23754e816156
fbarrailla <francois.barrailla@gmail.com>
2017-03-22 21:37:04 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+
+const child_process = require('child_process')
+const inquirer      = require('inquirer')
+
+const exec = command => new Promise((resolve, reject) =>
+  child_process.exec(command, (err, stdout) => err ? reject(err) : resolve(stdout)))
+
+const cleanBranchName = name => name.replace(/^\*/, '')
+
+const ask = (title, items) => inquirer.prompt(
+  {
+    type:    'list',
+    name:    'value',
+    message: title,
+    choices: items,
+  }
+).then(choice => choice.value)
+
+const selectBranch = () => exec('git branch')
+  .then(stdout   => stdout.split('\n').map(b => b.trim()).filter(Boolean))
+  .then(branches => branches.length ? ask('Select a branch', branches) : Promise.reject('No branches found...'))
+  .then(branch   => selectAction(cleanBranchName(branch)))
+
+const selectAction = branch => ask('What would you like to do ?', ['checkout', 'delete'])
+  .then(action => {
+    switch (action) {
+      case 'checkout':
+        return exec(`git co ${branch}`)
+      case 'delete':
+        return deleteBranch(branch)
+    }
+    return Promise.resolve()
+  })
+
+const deleteBranch = branch => ask('Are you sure ?', ['yes', 'no'])
+  .then(response => {
+    if (response === 'yes') {
+      return exec(`git branch -D ${branch}`)
+    }
+    return selectBranch()
+  })
+
+// 🏁 Let's Go 🏁
+selectBranch()
+  .catch(err => {
+    console.log(`❌  ${err}`)
+  })
+  .then(() => {
+    process.exit(0)
+  })
